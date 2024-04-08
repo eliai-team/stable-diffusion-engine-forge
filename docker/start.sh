f7cab3b08858baa3f1f1e01799c0d95f5336acd9
Nhat Do Minh <minhnhatdo0709@gmail.com>
2024-04-08 12:14:19 +0700

    docker build update

--- docker/start.sh
+++ docker/start.sh
@@ -13,7 +13,12 @@ echo "Worker Initiated"
 
 export SUPABASE_ENDPOINT=https://rtfoijxfymuizzxzbnld.supabase.co SUPABASE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0Zm9panhmeW11aXp6eHpibmxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1Nzc4MTQsImV4cCI6MjAxMjE1MzgxNH0.ChbqzCyTnUkrZ8VMie8y9fpu0xXB07fdSxVrNF9_psE AWS_ACCESS_KEY_ID=445b3a76828604585e2f38f49b39188b AWS_SECRET_ACCESS_KEY=9d5f008e8be8a9990a6cff7c6a1c78fc6498787a022cb0ea759cbd0af30c1848 MAX_MEMORY_CAPACITY=20 AWS_ENDPOINT=https://2c4e16b2cfe75a3201f2f7638084e66b.r2.cloudflarestorage.com STORAGE_DOMAIN=https://eliai-server.eliai.vn/
 
-cd ~/stable-diffusion-webui
+cd /stable-diffusion-webui
+
+git clone https://github.com/minhnhat0709/eliai-engine-sd-webui-ext extensions/eliai-engine-sd-webui-ext
+cd extensions/eliai-engine-sd-webui-ext
+git checkout auto-scale-engine
+cd ../..
 
 while true; do
 	if ! lsof -i:5456 -sTCP:LISTEN > /dev/null

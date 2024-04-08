dd27ccd86ba74dde3b586ed08cda22b5dda3d570
Nhat Do Minh <minhnhatdo0709@gmail.com>
2024-04-08 17:18:08 +0700

    path fix

--- docker/start.sh
+++ docker/start.sh
@@ -13,7 +13,7 @@ echo "Worker Initiated"
 
 export SUPABASE_ENDPOINT=https://rtfoijxfymuizzxzbnld.supabase.co SUPABASE_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0Zm9panhmeW11aXp6eHpibmxkIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1Nzc4MTQsImV4cCI6MjAxMjE1MzgxNH0.ChbqzCyTnUkrZ8VMie8y9fpu0xXB07fdSxVrNF9_psE AWS_ACCESS_KEY_ID=445b3a76828604585e2f38f49b39188b AWS_SECRET_ACCESS_KEY=9d5f008e8be8a9990a6cff7c6a1c78fc6498787a022cb0ea759cbd0af30c1848 MAX_MEMORY_CAPACITY=20 AWS_ENDPOINT=https://2c4e16b2cfe75a3201f2f7638084e66b.r2.cloudflarestorage.com STORAGE_DOMAIN=https://eliai-server.eliai.vn/
 
-cd ~/stable-diffusion-webui
+cd stable-diffusion-webui
 
 git clone https://github.com/minhnhat0709/eliai-engine-sd-webui-ext extensions/eliai-engine-sd-webui-ext
 cd extensions/eliai-engine-sd-webui-ext
@@ -23,12 +23,12 @@ cd ../..
 while true; do
 	if ! lsof -i:5456 -sTCP:LISTEN > /dev/null
 	then
-    		./webui.sh --nowebui --api --xformers --port 5456 > ~/engine_1.txt 2>&1 &
+    		./webui.sh --nowebui --api --xformers --port 5456 > engine_1.txt 2>&1 &
 	fi
 
 	if ! lsof -i:5002 -sTCP:LISTEN > /dev/null
 	then
-    		./webui.sh --nowebui --api --xformers --port 5002 > ~/engine_2.txt 2>&1 &
+    		./webui.sh --nowebui --api --xformers --port 5002 > engine_2.txt 2>&1 &
 	fi
 	
 	sleep 1m

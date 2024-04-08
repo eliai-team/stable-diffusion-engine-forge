8b11878ae9fc994f2f78fab744816aa0325f19ef
John Doe <johndoe@example.com>
2024-04-08 14:46:05 +0000

    test

---
+++ docker/install.sh
@@ -0,0 +1,14 @@
+cd /stable-diffusion-webui
+
+
+
+while true; do
+	if ! lsof -i:5456 -sTCP:LISTEN > /dev/null
+	then
+    	./webui.sh --nowebui --api --xformers --port 5456 > /engine_1.txt 2>&1 &
+	else
+      break
+  fi
+	
+	sleep 1m
+done

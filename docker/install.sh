7d00af37150467733e9aadb5f8497947b9a628ff
Nhat Do Minh <minhnhatdo0709@gmail.com>
2024-04-09 16:40:36 +0700

    build

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,7 @@
 cd /stable-diffusion-webui
 
-python -c "from modules import launch_utils; launch_utils.prepare_environment();" --skip-torch-cuda-test
+python -c "from modules import launch_utils; launch_utils.prepare_environment();" --skip-torch-cuda-test --xformers
+pip cache purge
 # ./webui.sh --nowebui --api --xformers --port 5456 --skip-torch-cuda-test --building
 
 # while true; do
